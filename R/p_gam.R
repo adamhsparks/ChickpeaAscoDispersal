6bfd59c6e4c248b4dd518df0cfc1d63201b98a7b
Adam H. Sparks <adamhsparks@gmail.com>
2020-03-17 21:36:56 +1000

    use \donttest until I can fix the example

--- R/p_gam.R
+++ R/p_gam.R
@@ -7,11 +7,13 @@
 #' @export p_gam
 #'
 #' @examples
+#' \donttest{
 #' library(mgcv)
 #' set.seed(2) ## simulate some data...
 #' dat <- gamSim(1,n=400,dist="normal",scale=2)
 #' b <- gam(y~s(x0)+s(x1)+s(x2)+s(x3),data=dat)
 #' p_gam(b)
+#' }
 
 p_gam <- function(x) {
    graphics::plot(x, allTerms = T) +
