bce9b6a420bb5a19276fe08d4d082ccc6584f3b0
Adam H. Sparks <adamhsparks@gmail.com>
2020-03-24 14:26:17 +1000

    Add cleaned weather docs

---
+++ R/cleaned_weather.R
@@ -0,0 +1,16 @@
+#' Cleaned Weather Data from Horsham and Curyo
+#'
+#' A dataset of weather data from on-site weather stations at Horsham and Curyo.
+#'
+#' @format A \code{data.frame} with 2202 rows and 6 variables:
+#' \describe{
+#'   \item{site}{Location where trial was located}
+#'   \item{rep}{Replicate number. The sequential number of rain or irrigation
+#'    event for a given site}
+#'   \item{time}{Date in POSIXct, format YYYY-MM-DD HH:MM:SS}
+#'   \item{wind_speed}{Recorded wind speed in metres per second}
+#'   \item{wind_direction}{Recorded wind direction in compass degrees}
+#'   \item{rainfall}{Recorded precipitation in millimetres}
+#' }
+#' @source Dr Joshua Fanning, AgricultureVictoria, Horsham, Vic, AUS
+"cleaned_weather"
